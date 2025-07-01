83498d5217634d9af1e2fc27d76978036f69ff42
nguyenhongdien <132563944+nguyenhongdien@users.noreply.github.com>
2025-07-01 16:27:51 +0700

    #RX-14: Order cart (xóa khoảng trắng, chỉnh connection string)

--- RestX.WebApp/Models/ViewModels/UniversalValue.cs
+++ RestX.WebApp/Models/ViewModels/UniversalValue.cs
@@ -5,7 +5,6 @@
         public T? Data { get; set; }
         public string? SuccessMessage { get; set; }
         public string? ErrorMessage { get; set; }
-
         public static UniversalValue<T> Success(T data, string success) => new UniversalValue<T> { Data = data, SuccessMessage = success};
         public static UniversalValue<T> Failure(string error) => new UniversalValue<T> { ErrorMessage = error };
     }
